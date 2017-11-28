cbd6a657b11bcf034b3e2ebe53ad4a0a99bf076a
Ivan Obeso <ivan.obeso1@gmail.com>
2017-11-28 00:38:18 +0000

    some workaround the traits

--- src/main/scala/genetic/common/Traits.scala
+++ src/main/scala/genetic/common/Traits.scala
@@ -5,16 +5,35 @@ import genetic.common.Types.Binary
 object Traits {
 
   trait Codificable[T]{
-    lazy val binaryRepr: Binary
     def encode():Binary
     def decode(bin: Binary):T
+    def binaryLength():Int
   }
 
-  trait Gen extends Codificable[Gen]{
+  trait Gen[A <: AnyVal] extends Codificable[Gen]{
+    val a: A
   }
 
-  trait Chromosome extends Codificable[Chromosome]{
-    val genes: List[Gen]
+  abstract case class Chromosome(val genes: List[Gen]) extends Codificable[Chromosome]{
+
+    def encode():Binary = {
+      genes.foldRight(List[Binary]())((x, acc) => List(x.encode()):::acc)
+    }
+
+    def decode(bin: Binary):Chromosome = {
+      def go(genes: List[Gen], bin: Binary): List[Gen] = genes match {
+        case x :: xs =>
+        case Nil => Nil
+      }
+      //val g = genes.foldRight(x => x.decode(bin.slice(0, x.binaryLength())))
+      val g = go(bin, genes)
+      Chromosome(g)
+    }
+
+    def binaryLength():Int = {
+      genes.foldRight(0)((x, acc) => acc+x.binaryLength())
+    }
+
   }
 
   trait Individual extends Codificable[Individual] with Ordered[Individual]{
