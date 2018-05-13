5d5f21386fa01634b40b82f94cbe00bc21b47928
Ivan Obeso Agüera <ivan.obeso1@gmail.com>
2018-05-13 17:09:20 +0100

    removed old tests

--- src/test/scala/IntegrationModelSuite.scala
+++ src/test/scala/IntegrationModelSuite.scala
@@ -45,40 +45,4 @@ class IntegrationModelSuite extends FunSuite{
     }
   }
 
-  /*test("test definition of generation function") {
-    val bodyGen = List(WrapInt(1))
-    val bodyChromo = List(bodyGen)
-    val bodyInd = List(bodyChromo)
-    def gen1 = Gen(bodyGen, (t: List[WrapType[_]])=>{true})
-    assert(true)
-   /* def chromo1 = Chromo(bodyChromo, (t: List[GenBody])=>{true})
-    def indiv1 = Individual(bodyInd, (t: List[ChromoBody])=>{true})*/
-  }
-
-  test("test random bodies generators") {
-    //Generation of a Gen with 3 bits of info
-    /*val genBody3 = genIntBodyRandomGenerator(3, ()=>1)
-    assert(genBody3 == List(WrapInt(1),WrapInt(1),WrapInt(1)))
-    //Generation of a chromosome with 5 Gens
-    val chromoBody5 = chromoBodyRandomGenerator(5, ()=>{genIntBodyRandomGenerator(3, ()=>1)})
-    assert(chromoBody5 == List(List(WrapInt(1), WrapInt(1), WrapInt(1)),
-      List(WrapInt(1), WrapInt(1), WrapInt(1)),
-      List(WrapInt(1), WrapInt(1), WrapInt(1)),
-      List(WrapInt(1), WrapInt(1), WrapInt(1)),
-      List(WrapInt(1), WrapInt(1), WrapInt(1))))
-    //Generation of an individual with 2 chromosomes
-    val indBody2 = individualBodyRandomGenerator(2, ()=>{chromoBodyRandomGenerator(5, ()=>{genIntBodyRandomGenerator(3, ()=>1)})})
-    assert(indBody2 == List(
-      List(List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1))),
-      List(List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)),
-        List(WrapInt(1), WrapInt(1), WrapInt(1)))))*/
-  }*/
-
 }
